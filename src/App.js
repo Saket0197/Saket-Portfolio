86b1ea2bf55de117ceb5379444766bf2702b18fb
Saket <saket.s9713@gmail.com>
2023-07-06 18:26:38 +0530

    added App Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Projects from "./components/Projects";
+import TechStack from "./components/TechStack";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className='container'>
+          <Header></Header>
+          <About></About>
+          <Projects></Projects>
+          <TechStack></TechStack>
+          <Footer></Footer>
+      </div>
     </div>
   );
 }
